93324ad2efc70c8ceede7576307e0b1fbfbdc9c5
Luca Di Marco <lucanahtsu@gmail.com>
2023-09-25 19:50:57 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.{html,js}',
+  ],
+  theme: {
+    colors: {
+      'lightred': '#ff5757',
+      'orangeyyellow': '#ffb01f',
+      'greenteal': '#00bd91',
+      'cobaltblue': '#1125d4',
+      'lighslateblue': '#7857ff',
+      'lightroyalblue': '#2e2be9',
+      'violetblue': '#4e21ca',
+      'persianblue': '#2421ca',
+      'white': '#ffffff',
+      'paleblue': '#ebf1ff',
+      'lightlavender': '#c8c7ff',
+      'darkgrayblue': '#303b5a'
+    },
+    extend: {
+      fontFamily: {
+        hanken: ['HankenGrotesk', 'sans'],
+      },
+    },
+  },
+  plugins: [],
+}
