a7e1bbeec851c50e21c94fcfa08e0c9c87dfc481
Luca Di Marco <lucanahtsu@gmail.com>
2023-09-26 15:36:41 -0300

    Move colors to extend so it works with javascript

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,23 @@ module.exports = {
     './*.{html,js}',
   ],
   theme: {
-    colors: {
-      'lightred': '#ff5757',
-      'orangeyyellow': '#ffb01f',
-      'greenteal': '#00bd91',
-      'cobaltblue': '#1125d4',
-      'lighslateblue': '#7857ff',
-      'lightroyalblue': '#2e2be9',
-      'violetblue': '#4e21ca',
-      'persianblue': '#2421ca',
-      'white': '#ffffff',
-      'paleblue': '#ebf1ff',
-      'lightlavender': '#c8c7ff',
-      'darkgrayblue': '#303b5a'
-    },
     extend: {
       fontFamily: {
         hanken: ['HankenGrotesk', 'sans'],
+      },
+      colors: {
+        'lightred': '#ff5757',
+        'orangeyyellow': '#ffb01f',
+        'greenteal': '#00bd91',
+        'cobaltblue': '#1125d4',
+        'lighslateblue': '#7857ff',
+        'lightroyalblue': '#2e2be9',
+        'violetblue': '#4e21ca',
+        'persianblue': '#2421ca',
+        'white': '#ffffff',
+        'paleblue': '#ebf1ff',
+        'lightlavender': '#c8c7ff',
+        'darkgrayblue': '#303b5a'
       },
     },
   },
